995dcf8e0c0245899cee2c2f5f5bf6b40203e2ff
jagra26 <jagra@ic.ufal.br>
2021-08-24 22:09:23 -0300

    loop folders

--- slicer.py
+++ slicer.py
@@ -12,33 +12,34 @@ from ripper import slice, save_tiles
 
 def get_prefix(filename, foldername):
     splitedFile = filename.split("\\")
-    splitedFile = splitedFile[1].split(".")
+    splitedFile = splitedFile[-1].split(".")
     splitedFolder = foldername.split("\\")
     return splitedFolder[-1] + "_" + splitedFile[0]
     
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--path", help="path to folder", default="testImages")
-parser.add_argument("-d", "--directory", help="output directory")
+#parser.add_argument("-d", "--directory", help="output directory")
 parser.add_argument("-c", "--collumns", type=int, help="number of collumns", default=8)
 parser.add_argument("-r", "--rows", type=int, help="number of rows", default=6)
 args = parser.parse_args()
 
-if args.directory is None:
-    if not os.path.exists(args.path + "Sliced"):
-        os.mkdir(args.path + "Sliced")
-    directory = args.path + "Sliced"
-else: directory = args.directory
 
-files = glob.glob(args.path+"/*")
+for folder in glob.glob(args.path + "/*"):
+    if not folder.endswith("Sliced"):
+        print(folder)
+        if not os.path.exists(folder + "Sliced"):
+            os.mkdir( folder + "Sliced")
+        directory = folder + "Sliced"
 
-count = 1
-with alive_bar(len(files)) as bar:
-    for i in range(len(files)):
-        try:
-            filename = files[i]
-            tiles = slice(filename, col=args.collumns, row=args.rows, save=False)
-            save_tiles(tiles, prefix=get_prefix(filename, args.path), directory=directory)
-            bar()
-        except Exception as e: print(e)
-print("end")
+        files = glob.glob(folder+"/*")
+
+        with alive_bar(len(files)) as bar1:
+            for i in range(len(files)):
+                try:
+                    filename = files[i]
+                    tiles = slice(filename, col=args.collumns, row=args.rows, save=False)
+                    save_tiles(tiles, prefix=get_prefix(filename, folder), directory=directory)
+                    bar1()
+                except Exception as e: print(e)
+        print("end")
